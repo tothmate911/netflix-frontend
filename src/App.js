4548f18a952e6a46985738e1daa36c8677689751
Tóth Máté <tothmate911@gmail.com>
2020-08-26 12:01:03 +0200

    add Main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import MainPage from './pages/MainPage';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>netflix</h1>
+      <BrowserRouter>
+        <Route>
+          <Route exact path="/" component={MainPage} />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/MainPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Welcome to our Video service!</h1>
+    </div>
+  );
+}
